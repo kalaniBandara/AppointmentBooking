13b719da9f35d007d5c098a5dc22ba84975a42a3
devlahirudinelka <devlahirudinelka@gmail.com>
2024-02-17 12:52:00 +0530

    appoiment type done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable react/no-unknown-property */
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(2);
+  const [selectedOption, setSelectedOption] = useState("just me");
 
+  // Function to handle change in the select element
+  const handleSelectChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+  function IncrementFun() {
+    if (count < 4) {
+      setCount(count + 1);
+    }
+  }
+  function DecrementFun() {
+    if (count <= 4 && count > 2) {
+      setCount(count - 1);
+    }
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="text-6xl">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section>
+        <div className="flex flex-col items-start justify-start bg-[#a8a3a3]  p-5 gap-5">
+          <p>Location</p>
+          <div className="w-full h-10 border-spacing-1 bg-[#fff]"></div>
+          <div className="w-full h-10 border-spacing-1 bg-[#fff] flex flex-row items-center justify-center gap-8">
+            <div className="basis-1/2">
+              <label for="book-type">
+                I would like to book an appointment for:
+              </label>
+              <select
+                name="book-type"
+                id="book-type"
+                value={selectedOption}
+                onChange={handleSelectChange}
+              >
+                <option value="justMe">Just Me</option>
+                <option value="2above">2-4 Guest</option>
+              </select>
+            </div>
+
+            <div className="basis-1/2">
+              {selectedOption == "2above" && (
+                <div className="flex flex-row items-center justify-center gap-5">
+                  <button onClick={DecrementFun}>-</button>
+                  <p>{count}</p>
+                  {count == 4 ? (
+                    <></>
+                  ) : (
+                    <button onClick={IncrementFun}>+</button>
+                  )}
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
